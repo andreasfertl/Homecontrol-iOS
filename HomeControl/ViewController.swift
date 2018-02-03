1317e0e0b88d319a1cd44faef4c431be72b97a4d
Andreas Fertl <andreas.fertl@luxenstein.de>
2018-02-03 15:16:11 +0100

    Initial Commit

---
+++ HomeControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeControl
+//
+//  Created by Andreas Fertl on 03.02.18.
+//  Copyright © 2018 Andreas Fertl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
